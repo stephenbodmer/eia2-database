5a67f0ed1814a5db667a462464e73112ba15fc2b
Stephen Bodmer <stephen.bodmer@hs-furtwangen.de>
2018-01-09 13:48:54 +0100

    .

---
+++ ServerTest.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var Http = require("http");
+var ServerTest;
+(function (ServerTest) {
+    var port = process.env.PORT;
+    if (port == undefined)
+        port = 8200;
+    var server = Http.createServer();
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
+    server.listen(port);
+    function handleListen() {
+        console.log("Server listening");
+    }
+    function handleRequest(_request, _response) {
+        console.log("Ich höre Stimmen!!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.write("Ich höre<br> Stimmen!!");
+        _response.end();
+    }
+})(ServerTest || (ServerTest = {}));
+//# sourceMappingURL=ServerTest.js.map
